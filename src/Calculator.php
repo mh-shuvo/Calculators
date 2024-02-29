aa4c31fa528fd8151b446733503a43cb2c769533
Md. Mehedi Hasan <30872044+mh-shuvo@users.noreply.github.com>
2024-02-29 18:25:01 +0600

    Create Calculator.php

---
+++ src/Calculator.php
@@ -0,0 +1,15 @@
+<?php
+namespace MhShuvo\Calculators;
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b)
+    {
+        return $a - $b;
+    }
+}
